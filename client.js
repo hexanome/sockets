3bc7c1fa4458f2ced7dc562b3add2e519c1acba5
Thaddee Tyl <thaddee.tyl@gmail.com>
2011-11-22 09:36:09 +0100

    read what user types

--- client.js
+++ client.js
@@ -1,14 +1,24 @@
 var net = require('net'),
-    socket = new net.Socket();
+    socket = new net.Socket(),
+    rl = require('readline'),
+    S = String;
 
 socket.connect(0x5AD);
 
-socket.write('hello!\n');
+socket.on('connect', function () {
+    
+    socket.write('BEGIN\n');
+    
+    rl.question('> ', function(tos) {
+      socket.write(tos);
+    });
 
-socket.on('data', function(data) {
-  var str = data.toString('utf8');
-  console.log(str);
-  if (str === 'END') {
-    socket.destroy();
-  }
+    socket.on('data', function(data) {
+      var str = S(data);
+      console.log(str);
+      if (str == 'END') {
+        socket.destroy();
+      }
+    });
+
 });
